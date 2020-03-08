1f8175b83c8ec264ec7cc043076435c38edbcc12
Steve Kinney <hello@stevekinney.net>
2020-03-08 16:52:08 -0600

    Initial commit

---
+++ src/Application.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Application.css';
+
+function Application() {
+  return (
+    <div className="Application">
+      <header className="Application-header">
+        <h1>Hello World!</h1>
+        <a href="https://twilio.com/jobs">
+          Like come work with us or something.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Application;
