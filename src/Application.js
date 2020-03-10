42b122acbb78b4c832ed84478f3258d30d89ce5b
Steve Kinney <hello@stevekinney.net>
2020-03-10 11:59:49 -0600

    Change the basics

--- src/Application.js
+++ src/Application.js
@@ -1,17 +1,44 @@
 import React from 'react';
+import uniqueId from 'lodash/uniqueId';
 import './Application.css';
 
-function Application() {
+const Application = () => {
+  const [badThings, setBadThings] = React.useState([]);
+
+  const addBadThing = ({ badThing }) => {
+    setBadThings([{ id: uniqueId(), value: badThing }, ...badThings]);
+  };
+
   return (
-    <div className="Application">
-      <header className="Application-header">
-        <h1>Hello World!</h1>
-        <a href="https://twilio.com/jobs">
-          Like come work with us or something.
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Bad Things I've Done</h1>
+      <NewBadThing onSubmit={addBadThing} />
+      {badThings.map(thing => (
+        <p key={thing.id}>{thing.value}</p>
+      ))}
+    </main>
   );
-}
+};
+
+const NewBadThing = ({ onSubmit }) => {
+  const [badThing, setBadThing] = React.useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    onSubmit({ badThing });
+    setBadThing('');
+  };
+
+  const handleChange = event => {
+    setBadThing(event.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={badThing} onChange={handleChange} />
+      <input type="submit" />
+    </form>
+  );
+};
 
 export default Application;
